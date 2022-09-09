5bddd0a5ebd0ea2d6db6fb3e3073dc75aa25c224
Tonny221 <anthonny14gabriell@gmail.com>
2022-09-09 15:52:59 -0400

    button

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import AcademicData from "./containers/AcademicData";
 import CourseInfoData from "./containers/CourseInfoData";
 import ProInfoData from "./containers/ProInfoData";
 import PCDInfo from "./containers/PCDInfo";
+import { Box, Button } from "@mui/material";
 
 function App() {
   return (
-    <>
+    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <Header />
       <PersonalData />
       <AcademicData />
       <CourseInfoData />
       <ProInfoData />
       <PCDInfo />
-    </>
+      <>
+        <Button variant="contained" sx={{width: '6rem', marginBottom: '2rem'}}>Enviar</Button>
+      </>
+    </Box>
   );
 }
 
