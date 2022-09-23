42724e1e6033d512c8d616c57a0c63567f6a82e7
Tonny221 <anthonny14gabriell@gmail.com>
2022-09-23 13:37:36 -0400

    little fix

--- src/containers/styles.js
+++ src/containers/styles.js
@@ -2,22 +2,19 @@ import { Box, styled, Paper, Button } from '@mui/material'
 
 export const PaperContainer = styled(Box)(({ theme }) => ({
     width: '60%',
-    [theme.breakpoints.down('sm')]: {
-        width: '100%'
-    },
     [theme.breakpoints.down('md')]: {
         width: '90%'
+    },
+    [theme.breakpoints.down('sm')]: {
+        width: '100%'
     },
     margin: '4rem auto'
 }));
 
 export const InfoPaper = styled(Paper)(({ theme }) => ({
-    padding: '1rem',
+    padding: '2rem',
     [theme.breakpoints.up('sm')]: {
         padding: '6rem'
-    },
-    [theme.breakpoints.down('md')]: {
-        padding: '4rem'
     },
     display: 'flex',
     flexDirection: 'column',
